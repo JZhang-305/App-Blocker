a3378d1909324f75e292f01ba3d4abe5d165bd8b
JZhang-305 <jzhang.of.ny@gmail.com>
2022-12-26 19:09:15 -0500

    Make app list more efficient by comparing appNames instead of ApplicationInfo object.

--- app/src/main/java/com/example/appblocker/ui/InstalledAppsList.kt
+++ app/src/main/java/com/example/appblocker/ui/InstalledAppsList.kt
@@ -17,10 +17,25 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun InstalledAppsList(context: Context, modifier: Modifier = Modifier) {
-    val packageManager = context.packageManager
-    val installedApps = packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
 
-    val checkedApps = remember { mutableStateListOf<ApplicationInfo>() }
+
+
+
+    val packageManager = context.packageManager
+    val listOfApps = packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
+    // For speeding up process of displaying all apps
+    val installedApps = mutableMapOf<String, ApplicationInfo>()
+    for (app in listOfApps) {
+        if (app.flags and ApplicationInfo.FLAG_SYSTEM == 0) {
+            installedApps[app.loadLabel(packageManager).toString()] = app
+        }
+    }
+    val listOfAppNames = mutableListOf<String>()
+    for ((key, value) in installedApps) {
+        listOfAppNames.add(key)
+    }
+
+    val checkedApps = remember { mutableStateListOf<String>() }
 
     //val checkedApps = remember { mutableStateMapOf<ApplicationInfo>() }
 
@@ -28,29 +43,31 @@ fun InstalledAppsList(context: Context, modifier: Modifier = Modifier) {
         bottomBar = {
             BottomAppBar {
                 Button(
-                    //modifier = modifier.align(Alignment.CenterHorizontally),
                     onClick = {
                         // Save the list of checked apps
 
+                        /*
                         val sharedPreferences = context.getSharedPreferences("app_blocker", Context.MODE_PRIVATE)
                         val editor = sharedPreferences.edit()
                         val packageNames = checkedApps.map { it.packageName }
                         editor.putStringSet("checked_apps", packageNames.toSet())
-                        editor.apply()
+                        editor.apply()*/
+
                     }) {
-                    Text("Save")
-                    for (i in checkedApps) {
-                        Text(i.loadLabel(packageManager).toString())
+                    Text("+")
+                    for (app in checkedApps) {
+                        //Text(i.loadLabel(packageManager).toString())
+                        Text(app)
                     }
                 }
             }
         }
     ) {
+
         LazyColumn {
-            items(installedApps) { app ->
-                if (app.flags and ApplicationInfo.FLAG_SYSTEM == 0) {
-                    InstalledAppRow(app, packageManager, checkedApps)
-                }
+            items(listOfAppNames) { app ->
+                InstalledAppRow(app, packageManager, checkedApps)
+
             }
         }
     }
@@ -59,23 +76,24 @@ fun InstalledAppsList(context: Context, modifier: Modifier = Modifier) {
 
 
 @Composable
-fun InstalledAppRow(app: ApplicationInfo,
+fun InstalledAppRow(appName: String,
                     packageManager: PackageManager,
-                    checkedApps: MutableList<ApplicationInfo>) {
-    val appIcon = app.loadIcon(packageManager)
-    Row {
+                    checkedApps: MutableList<String>) {
+    // val appIcon = app.loadIcon(packageManager)
+    //val appName = app.loadLabel(packageManager).toString()
+    Row (verticalAlignment = Alignment.CenterVertically) {
         Checkbox(
-            checked = checkedApps.contains(app),
+            checked = checkedApps.contains(appName),
             onCheckedChange = { newlyChecked ->
                 if (newlyChecked) {
-                    checkedApps.add(app)
+                    checkedApps.add(appName)
                 } else {
-                    checkedApps.remove(app)
+                    checkedApps.remove(appName)
                 }
             }
         )
 
-        Text(app.loadLabel(packageManager).toString(), fontSize = 15.sp, fontWeight = FontWeight.Bold)
+        Text(appName, fontSize = 15.sp, fontWeight = FontWeight.Bold)
 
         Spacer(modifier = Modifier.height(50.dp))
     }
