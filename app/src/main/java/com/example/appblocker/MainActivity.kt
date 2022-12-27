1cf9570e38807d9e3f29fb547ae551a59a35fea1
JZhang-305 <jzhang.of.ny@gmail.com>
2022-12-27 14:11:39 -0500

    Simplify MainActivity.kt

--- app/src/main/java/com/example/appblocker/MainActivity.kt
+++ app/src/main/java/com/example/appblocker/MainActivity.kt
@@ -1,85 +1,18 @@
 package com.example.appblocker
 
-import com.example.appblocker.ui.InstalledAppsList
-import android.annotation.SuppressLint
+import com.example.appblocker.ui.PlaylistSelectorScreen
 import android.content.Context
-import android.content.pm.ApplicationInfo
-import android.content.pm.PackageManager
-//import androidx.compose.foundation.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.appblocker.ui.theme.AppBlockerTheme
-import android.content.pm.PackageInfo
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.drawable.Drawable
-import android.media.Image
-import android.text.Layout
-import android.widget.ImageView
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat
-import java.lang.IndexOutOfBoundsException
-
-//import androidx.compose.Composable
-/*import androidx.ui.core.Modifier
-import androidx.ui.foundation.Text
-import androidx.ui.layout.Column
-import androidx.ui.layout.padding
-import androidx.ui.material.Checkbox
-import androidx.ui.material.MaterialTheme
-import androidx.ui.unit.dp*/
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            /*AppBlockerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    ShowApps(this)
-                }
-            }*/
-            MyApp(this)
+            PlaylistSelectorScreen(this)
         }
-    }
-}
-@Composable
-fun MyApp(context: Context) {
-    InstalledAppsList(context)
-}
-
-
-
-
-
-data class InstalledApp(val appName: String, val packageName: String, var isChecked: Boolean)
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AppBlockerTheme {
-
     }
 }
