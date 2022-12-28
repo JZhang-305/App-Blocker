d84cade6015d697add9b8f08088a0a8fa84956a1
JZhang-305 <jzhang.of.ny@gmail.com>
2022-12-28 03:33:10 -0500

    Add app dropdown to PlaylistScreen.kt.

--- app/src/main/java/com/example/appblocker/ui/PlaylistScreen.kt
+++ app/src/main/java/com/example/appblocker/ui/PlaylistScreen.kt
@@ -1,13 +1,16 @@
 package com.example.appblocker.ui
 
 import android.content.Context
+import android.content.SharedPreferences
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Cyan
@@ -25,10 +28,9 @@ fun PlaylistScreen(context: Context, navController: NavHostController, modifier:
     Scaffold(
         bottomBar = {
             BottomAppBar {
-                // When clicked, button should save a new playlist and navigate back to screen displaying all playlists
+                // When clicked, button should navigate to PlaylistSelectorScreen to make a new playlist
                 Button(
                     onClick = {
-
                         navController.navigate("PlaylistSelectorScreen")
                     },
                     colors = ButtonDefaults.buttonColors(backgroundColor = Color.Yellow)
@@ -39,15 +41,37 @@ fun PlaylistScreen(context: Context, navController: NavHostController, modifier:
         }
     ) {
 
-        // TEST CODE FOR DISPLAYING DATABASE, DO NOT KEEP
-        //Text(database.getAll().toString())
-
         val playlistNames = database.getAll().keys.toList()
         LazyColumn {
             items(playlistNames) { playlist ->
-                Text(playlist, fontSize = 30.sp, fontWeight = FontWeight.Bold)
-                Spacer(modifier = Modifier.height(50.dp))
+                //Divider(color = Color.Gray)
+                PlaylistRow(playlist = playlist, database = database)
+                Divider(color = Color.Gray, )
             }
         }
+    }
+}
+
+@Composable
+fun PlaylistRow(playlist: String, database: SharedPreferences, modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false) }
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Text(playlist, fontSize = 30.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.width(50.dp))
+        Button(onClick = {expanded = true}) {Text("Apps")}
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            for (appInPlaylist in database.getStringSet(playlist, mutableSetOf())?.toList()?.sorted()!!) {
+                DropdownMenuItem(onClick = {}) {
+                    Text(appInPlaylist)
+                }
+                Divider()
+            }
+        }
+
+        // Insert garbage can button that deletes
+        Spacer(modifier = Modifier.height(50.dp))
     }
 }
